9e030a35ea5a7f0ad084de8007909e7d1ea6cd2d
Asus <Asus@LAPTOP-V605S04R>
2022-06-29 14:49:28 +0300

    Добавьте файлы проекта.

---
+++ PracticalWork_5.1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork_5._1
+{
+    internal class Program
+    {
+        
+        static string[] GetSeparateWord(string n)
+        {
+            string[] words = n.Split(' ');
+            return words;
+        }
+
+        static void OutputWords(string[] words)
+        {
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void TranspositionWords(string[] words)
+        {
+            string NewSentence = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                NewSentence += words[words.Length - 1 - i] + " ";
+            }
+            Console.WriteLine(NewSentence);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+
+            #region Задание 1. Метод разделения строки на слова
+            
+            string n = Console.ReadLine();
+            string[] words = GetSeparateWord(n);
+
+            GetSeparateWord(n);
+            OutputWords(words);
+            #endregion
+
+            #region Задание 2. Перестановка слов в предложении
+
+            //string n = Console.ReadLine();
+            //TranspositionWords(GetSeparateWord(n));
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
